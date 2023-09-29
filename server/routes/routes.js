3f8cca0a84702652d03d66bccdd42274ec9c94d4
Romar Macaraeg <roamrcm@gmail.com>
2023-09-30 04:06:14 +0800

    Update

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,19 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/biography", (req, res) => {
-  res.send("biography");
+  res.sendFile("biography.html", { root: __dirname + "../../../public/views" });
 });
 
 router.get("/legacy", (req, res) => {
-  res.send("legacy");
-});
-
-router.get("/gallery", (req, res) => {
-  res.send("gallery");
-});
-
-router.get("/contact", (req, res) => {
-  res.send("contact");
+  res.sendFile("legacy.html", { root: __dirname + "../../../public/views" });
 });
 
 module.exports = router;
